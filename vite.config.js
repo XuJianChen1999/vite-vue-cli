f638c00c124a4723e730b951468a9bdfadee41ea
徐建辰 <18253997748@163.com>
2022-06-08 09:44:22 +0800

    配置vite：添加打包分析、代理、路径别名、代理等等

---
+++ vite.config.js
@@ -0,0 +1,61 @@
+import {fileURLToPath, URL} from 'url'
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import {visualizer} from 'rollup-plugin-visualizer'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [
+    vue(), 
+    vueJsx(),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+  },
+  server: {
+    proxy: {
+      '/agency': {
+        target: 'https://xxxxx.com/api',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/agency/, ''),
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      }
+    },
+    rollupOptions: {
+      output: {
+        // 拆包
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        assetFileNames: '[ext]/[name]-[hash].[ext]',
+        // 第三方库拆包
+        manualChunks: {
+          xgplayer: ['xgplayer'],
+          echarts: ['echarts'],
+          tinymce: ['tinymce'],
+          elicons: ['elicons']
+        },
+      },
+    }
+  }
+})
